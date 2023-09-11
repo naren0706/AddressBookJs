0958e4164e74c67a7cc5a8d99200b5815071b0d7
naren0706 <narenthrakishhore@gmail.com>
2023-09-11 12:02:52 +0530

    [Narenthra Kishore] Add : validation to the given input

--- Contact.js
+++ Contact.js
@@ -1,6 +1,14 @@
-
 class Contact {
     constructor(firstName, lastName, address, city, state, zip, phoneNumber, email) {
+        this.validateName(firstName, 'First Name');
+        this.validateName(lastName, 'Last Name');
+        this.validateLength(address, 'Address', 4);
+        this.validateLength(city, 'City', 4);
+        this.validateLength(state, 'State', 4);
+        this.validatePattern(zip, 'Zip Code', /^\d{5}$/);
+        this.validatePattern(phoneNumber, 'Phone Number', /^\d{10}$/);
+        this.validatePattern(email, 'Email', /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/);
+
         this.firstName = firstName;
         this.lastName = lastName;
         this.address = address;
@@ -9,6 +17,24 @@ class Contact {
         this.zip = zip;
         this.phoneNumber = phoneNumber;
         this.email = email;
+    }
+
+    validateName(name, fieldName) {
+        if (!/^[A-Z][a-zA-Z]{2,}$/.test(name)) {
+            throw new Error(`${fieldName} should start with a capital letter and have at least 3 characters.`);
+        }
+    }
+
+    validateLength(value, fieldName, minLength) {
+        if (value.length < minLength) {
+            throw new Error(`${fieldName} should have at least ${minLength} characters.`);
+        }
+    }
+
+    validatePattern(value, fieldName, pattern) {
+        if (!pattern.test(value)) {
+            throw new Error(`Invalid ${fieldName}.`);
+        }
     }
 }
 
--- Operation.js
+++ Operation.js
@@ -7,6 +7,7 @@ const addressBook = new AddressBook();
 const contact1 = new Contact("John", "Doe", "123 Main St", "New York", "NY", "10001", "555-555-5555", "john@example.com");
 const contact2 = new Contact("Jane", "Smith", "456 Elm St", "Los Angeles", "CA", "90001", "555-555-1234", "jane@example.com");
 
+
 addressBook.addContact(contact1);
 addressBook.addContact(contact2);
 
