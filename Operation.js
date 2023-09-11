79395a7aa3405ce39f34ff3ff1d80b9def1b06bb
naren0706 <narenthrakishhore@gmail.com>
2023-09-11 12:37:25 +0530

    [Narenthra Kishore] Add : add new contact

--- Operation.js
+++ Operation.js
@@ -6,6 +6,7 @@ const addressBook = new AddressBook();
 
 const contact1 = new Contact("John", "Doe", "123 Main St", "New York", "NY", "10001", "555-555-5555", "john@example.com");
 const contact2 = new Contact("Jane", "Smith", "456 Elm St", "Los Angeles", "CA", "90001", "555-555-1234", "jane@example.com");
+const contact3 = new Contact("Naren", "Rs", "123", "Sa", "CAasd", "123456", "123213123213", "Naren@example.com");
 
 
 addressBook.addContact(contact1);
---
+++ python.py
@@ -0,0 +1,33 @@
+import subprocess
+
+# Define the Git Bash command you want to run
+# Replace this with your desired Git Bash command
+def function(git_command):
+    # Run the Git Bash command
+    try:
+        result = subprocess.run(git_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        
+        # Check if the command was successful
+        if result.returncode == 0:
+            print("Command executed successfully:")
+            print(result.stdout)
+        else:
+            print("Command failed with error:")
+            print(result.stderr)
+    except Exception as e:
+        print("An error occurred:", str(e))
+        
+def createUC(UC,msg):
+    function("git checkout -b"+UC)
+    function("git add .")
+    function( "git commit -m \" [Narenthra Kishore] Add : "+msg+"\"")
+    function("git push "+UC)
+    function("git checkout main")
+    function("git merge "+UC)
+    function("git push origin main ")
+    return;
+        
+print("Create UC and push with a commit message and merge it with main ")
+UCname=input("Enter the UseCaseName:")
+commitmessage=input("Enter the commit message :")
+createUC(UCname,commitmessage)
